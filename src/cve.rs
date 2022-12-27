ac356f9065a54a7c7e8aff3e68e5bde95bfb3eb7
赵光建 <zhaoguangjian@cyberkl.com>
2022-12-27 21:10:32 +0800

    sync cve

--- src/cve.rs
+++ src/cve.rs
@@ -1,11 +1,12 @@
 use std::{
     fs::{self, File},
-    io::Write,
+    io::{BufReader, Read, Write},
     path::{Path, PathBuf},
 };
 
 use chrono::{Datelike, Local};
 use futures::future::join_all;
+use sha2::{Digest, Sha256};
 
 static DATA_DIR: &str = "./data";
 
@@ -18,28 +19,53 @@ async fn load_db() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn sync_db(path_dir: PathBuf) -> Result<Vec<File>, Box<dyn std::error::Error>> {
+async fn sync_cve(path_dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     let year_start = 2002;
     let year_now = Local::now().year();
     let file_count = year_now - year_start + 1;
     let mut future_list = Vec::with_capacity(file_count.try_into().unwrap());
-    let mut file_list = Vec::with_capacity(file_count.try_into().unwrap());
     for year in year_start..(year_now + 1) {
-        let file_name = format!("nvdcve-1.1-{}.json.gz", year);
-        let url = format!("https://nvd.nist.gov/feeds/json/cve/1.1/{}", file_name);
-        let file_gz = File::create(path_dir.join(file_name)).unwrap();
-        file_list.push(file_gz.try_clone().unwrap());
-        let future_download = download(url, file_gz);
+        let future_download = download(year, path_dir.to_owned());
         future_list.push(future_download);
     }
     let _ = join_all(future_list).await;
-    Ok(file_list)
+    Ok(())
 }
 
-async fn download(url: String, mut file_local: File) -> Result<(), Box<dyn std::error::Error>> {
-    let rsp = reqwest::get(url).await?;
-    let rsp_bytes = rsp.bytes().await?;
-    file_local.write_all(&rsp_bytes)?;
+async fn download(year: i32, path_dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    let url_meta = format!(
+        "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-{}.meta",
+        year
+    );
+    let rsp = reqwest::get(&url_meta).await?;
+    if rsp.status().is_success() {
+        let meta = rsp.text().await?;
+        let (_, sha256_lastest) = meta.trim_end().split_once("sha256:").unwrap();
+        let file_name_gz = format!("nvdcve-1.1-{}.json.gz", year);
+        let path_gz = path_dir.join(&file_name_gz);
+        if path_gz.exists() {
+            let file_gz = File::open(&path_gz).unwrap();
+            let gz_decoder = flate2::read::GzDecoder::new(file_gz);
+            let mut buf_reader = BufReader::new(gz_decoder);
+            let mut buf = Vec::new();
+            buf_reader.read_to_end(&mut buf).unwrap();
+            let sha256_local = hex::encode_upper(Sha256::digest(buf));
+            let sha256_local = sha256_local.as_str();
+            if sha256_local == sha256_lastest {
+                // no need to redownload
+                log::debug!("no need to redownload, same sha256: {}", sha256_lastest);
+                return Ok(());
+            }
+        }
+        let url_gz = format!("https://nvd.nist.gov/feeds/json/cve/1.1/{}", file_name_gz);
+        log::debug!("download: {}", &url_gz);
+        let rsp = reqwest::get(url_gz).await?;
+        let rsp_bytes = rsp.bytes().await?;
+        let mut file_gz = File::create(path_gz).unwrap();
+        file_gz.write_all(&rsp_bytes)?;
+    } else {
+        log::error!("get meta fail: {}", &url_meta);
+    }
     Ok(())
 }
 
@@ -56,11 +82,11 @@ async fn init_dir(data_dir: &str) -> Result<PathBuf, Box<dyn std::error::Error>>
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use chrono::Local;
-    use tracing_subscriber::fmt::{format::Writer, time::FormatTime};
+    use super::{init_dir, sync_cve, DATA_DIR};
 
     fn init_log() {
+        use chrono::Local;
+        use tracing_subscriber::fmt::{format::Writer, time::FormatTime};
         struct LocalTimer;
         impl FormatTime for LocalTimer {
             fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
@@ -88,13 +114,53 @@ mod tests {
         log::info!("dir {:?} initialized", path_dir);
         Ok(())
     }
-    // cargo test cve::tests::test_sync_db
+    // cargo test cve::tests::test_sync_cve
     #[tokio::test]
-    async fn test_sync_db() -> Result<(), Box<dyn std::error::Error>> {
+    async fn test_sync_cve() -> Result<(), Box<dyn std::error::Error>> {
         init_log();
         let path_dir = init_dir(DATA_DIR).await?;
-        let file_list = sync_db(path_dir).await?;
-        log::info!("file_list {:?}", file_list);
+        let _ = sync_cve(path_dir).await?;
         Ok(())
+    }
+    // cargo test cve::tests::it_works_async
+    #[tokio::test]
+    async fn it_works_async() -> Result<(), Box<dyn std::error::Error>> {
+        use sha2::{Digest, Sha256};
+        use std::fs::File;
+        use std::io::BufReader;
+        use std::io::Read;
+
+        init_log();
+        // 3B031264935B91768D8D6C9977FABF0D0F0E12E057435F93CE664326A62D66A5
+
+        let file_gz = File::open("./data/nvdcve-1.1-2022.json.gz").unwrap();
+        let gz_decoder = flate2::read::GzDecoder::new(file_gz);
+        let mut buf_reader = BufReader::new(gz_decoder);
+        let mut buf = Vec::new();
+        buf_reader.read_to_end(&mut buf).unwrap();
+        let sha256_local = hex::encode_upper(Sha256::digest(buf));
+        log::info!("GzDecoder sha256_local: {}", sha256_local);
+
+        Ok(())
+    }
+
+    // cargo test cve::tests::test_it_works
+    #[test]
+    fn test_it_works() {
+        // 68a85d83abdd0fb34a31d1c49bb0ecfa380597046528ce624c514ea311b7f59f
+        use sha2::{Digest, Sha256};
+        use std::fs::File;
+        use std::io::BufReader;
+        use std::io::Read;
+
+        init_log();
+
+        let file_gz = File::open("./data/nvdcve-1.1-2022.json.gz").unwrap();
+        let gz_decoder = flate2::read::GzDecoder::new(file_gz);
+        let mut buf_reader = BufReader::new(gz_decoder);
+        let mut buf = Vec::new();
+        buf_reader.read_to_end(&mut buf).unwrap();
+        let sha256_local = hex::encode_upper(Sha256::digest(buf));
+        log::info!("GzDecoder sha256_local: {}", sha256_local);
     }
 }
