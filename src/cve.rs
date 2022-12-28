6a7093b4175118ca84d0554a2a1bc4a19b5a0a55
赵光建 <zhaoguangjian@cyberkl.com>
2022-12-28 13:37:01 +0800

    update version

--- src/cve.rs
+++ src/cve.rs
@@ -1,23 +1,23 @@
 //! # nvd
 //! Some functions about CPE and CVE
-//! 
+//!
 //! ## cve
 //! - [x] download cve file,
 //! - [x] json to proto
 //! - [x] load proto
 //! - [ ] cpe match
-//! 
+//!
 //! # Usage
 //! Add this to your *Cargo.toml*:
 //! ```toml
 //! [dependencies]
 //! nvd = "0.1"
 //! ```
-//! 
+//!
 //! ### Examples
 //! ```rust
 //! use nvd::cve::{init_dir, init_log, load_db, make_db, sync_cve};
-//! 
+//!
 //! #[tokio::main]
 //! async fn main() -> Result<(), Box<dyn std::error::Error>> {
 //!     init_log();
